3fc429c1a99848954730c28f88fe0ff10c38fb78
nabliha-11 <nabliha@iut-dhaka.edu>
2021-10-31 13:53:43 +0600

    assignment

--- ClassroomCppVersion/TeacherState.cpp
+++ ClassroomCppVersion/TeacherState.cpp
@@ -15,6 +15,38 @@ void StateInfo::TeacherState::Init()
 void StateInfo::TeacherState::HandleInput()
 {
 	Display();
+	string choice;
+	cin >> choice;
+	if (choice == "#addstu")
+	{
+
+	}
+	if (choice == "#addmat")
+	{
+
+	}
+	if (choice == "#addout")
+	{
+
+	}
+	if (choice == "#addassign")
+	{
+
+	}
+	if (choice == "#addnotes")
+	{
+
+	}
+	if (choice == "#coursepar")
+	{
+		//displayParticipants();
+	}
+	if (choice == "#back")
+	{
+		data->machine.removeState();
+		//dashboardState(currentUser*);
+	}
+
 }
 
 void StateInfo::TeacherState::Pause()
@@ -32,4 +64,13 @@ void StateInfo::TeacherState::Display()
 	system("cls");
 	cout << "Welcome to Course: " << data->currentCourse.getCourseCode()<<endl;
 	cout <<"Teacher: "<< data->currentUser.getUsername() << endl;
+	cout << "Commands: " << endl
+		<< "To add student #addstu" << endl
+		<< "To add Course Materials #addmat" << endl
+		<< "To add Course Outline #addout" << endl
+		<< "To add assignment #addassign" << endl
+		<< "To add Short Notes #addsnotes" << endl
+		<< "To see course participants type #coursepar" << endl
+		<< "To go back type #back" << endl;
+
 }
