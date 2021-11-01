bdce5a0345ccbcb005151b48e4e57c122f161711
MD. Tanvir Hossain Saikat <tanvirhossain3@iut-dhaka.edu>
2021-11-01 11:35:50 +0600

    day 5 (11:35 AM) -- working on student state

--- ClassroomCppVersion/StudentState.cpp
+++ ClassroomCppVersion/StudentState.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 StateInfo::StudentState::StudentState(ProgramDataRef data) : data(data)
 {
-	HandleInput();
+	
 }
 
 
 void StateInfo::StudentState::Init()
 {
-
+	HandleInput();
 }
 
 void StateInfo::StudentState::HandleInput()
 {
 	Display();
+	string choice;
+	cin >> choice;
+
+
 }
 
 void StateInfo::StudentState::Pause()
@@ -30,4 +34,11 @@ void StateInfo::StudentState::Resume()
 void StateInfo::StudentState::Display()
 {
 	system("cls");
+	cout << "Username: " << data->currentUser.getUsername() << endl;
+	cout << "Class code: " << data->currentCourse.getCourseCode() << endl;
+	cout << "Commands: " << endl;
+	cout << "#seepost - To see class posts" << endl;
+	cout << "#seeassignment - To see class assignment" << endl;
+	cout << "#seemat - To see course materials" << endl;
+	cout << "#seeoutline - To see course outline" << endl;
 }
